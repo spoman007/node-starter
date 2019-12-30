605f27728ab19da4c704968d4e976649fa6c878c
DEV-POMANS <DEV-POMANS@iaea.org>
2019-12-30 18:13:06 +0100

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from "express"
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express()
+
+app.use(cors())
+
+const port = process.env.PORT || 3000
+
+
+app.get('/', (req, res) => {
+    return res.send('Received a GET HTTP method');
+});
+app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+});
+app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+});
+app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
