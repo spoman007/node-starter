ef0cf8526e30cfafec9571d92e370397bd3ba3c3
Sandeep Poman <S.S.Poman@iaea.org>
2020-09-10 17:47:34 +0200

    Adding the async await ability

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ app.use(cors())
 
 const port = process.env.PORT || 3000
 
-
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     return res.send('Received a GET HTTP method');
 });
 app.post('/', (req, res) => {
